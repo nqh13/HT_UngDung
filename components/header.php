92bc44163e20bd22aafb88eb53a7aa1fc2bdf8e2
tlr14 <thailamtruong05@gmail.com>
2023-11-03 20:04:04 +0700

    index.php

---
+++ components/header.php
@@ -0,0 +1,99 @@
+<div class="container-fluid bg-light hi">
+      <h6>Chào mừng bạn đến với bếp ăn tập đoàn!</h6>
+    </div>
+    <header>
+      <div class="container-fluid">
+        <div class="row main-header">
+          <div class="col-lg-4 col-md-4 col-sm-12">
+            <a href="index.html"
+              ><img class="logo" src="./img/LOGO-01.png" alt="Logo"
+            /></a>
+          </div>
+          <div class="col-lg-4 col-md-4 col-sm-12 time">
+            <div class="clock">
+              <i
+                class="fa-regular fa-clock"
+                style="color: #080808; font-size: 50px"
+              ></i>
+            </div>
+            <div class="time-content">
+              <span class="time-title">OPEN:</span><br />
+              <span class="time-info">10AM - 17PM</span>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-4 col-sm-12 user">
+            <i class="fa-solid fa-user fa-2xl"></i>
+            <div class="title-user">
+              <ul class="nav">
+                <li class="nav-item dropdown">
+                  <a
+                    class="dropdown-toggle"
+                    data-bs-toggle="dropdown"
+                    style="color: #080808; text-decoration: none"
+                    href="#"
+                    >User</a
+                  >
+                  <ul class="dropdown-menu">
+                    <li>
+                      <a class="dropdown-item" href="#">Xem Phiếu đặt món</a>
+                    </li>
+                    <li><a class="dropdown-item" href="#">Xem công nợ</a></li>
+                    <li><a class="dropdown-item" href="#">Đề xuất món</a></li>
+                    <li><a class="dropdown-item" href="#">Đăng xuất</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+      <div class="navbar-header">
+        <button
+          type="button"
+          class="navbar-toggle"
+          data-toggle="collapse"
+          data-target="#myNavbar"
+        >
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+      </div>
+
+      <div class="collapse navbar-collapse" id="mynavbar">
+        <ul class="navbar-nav me-auto">
+          <li class="nav-item menu1">
+            <a class="nav-link" href="#">TRANG CHỦ</a>
+          </li>
+          <li class="nav-item menu">
+            <a class="nav-link" href="./html/thucdon.html">THỰC ĐƠN</a>
+          </li>
+          <li class="nav-item menu">
+            <a class="nav-link" href="./html/gioithieu.html">GIỚI THIỆU</a>
+          </li>
+          <li class="nav-item menu">
+            <a class="nav-link" href="./html/lienhe.html">LIÊN HỆ</a>
+          </li>
+        </ul>
+        <form class="d-flex search">
+          <input
+            class="form-control me-2"
+            type="text"
+            placeholder="Bạn cần tìm gì???"
+          />
+          <button class="btn btn-primary" type="button">
+            <i class="fa-solid fa-magnifying-glass" style="color: #ffffff"></i>
+          </button>
+        </form>
+        <div class="cart">
+          <a href="./html/datmon.html"
+            ><i
+              class="fa-solid fa-cart-shopping fa-2xl"
+              style="color: #fcfcfd"
+            ></i
+          ></a>
+        </div>
+      </div>
+    </nav>
